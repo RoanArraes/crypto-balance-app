e1145a0984d72fd60dceff0f4b44cf053fb367e6
RoanArraes <roan.arraes@hotmail.com>
2022-02-22 22:37:35 -0300

    adjust constants

--- src/utils/constants/labels.tsx
+++ src/utils/constants/labels.tsx
@@ -2,7 +2,8 @@ export const LABELS = {
   POWERED_BY: "Powered by Roan.Arraes",
   TITLE_PROJECT: "Crypto Balance",
   PORTFOLIO_TABLE_TITLE: "Portfolio",
-  PORTFOLIO_USER: "Your Portfolios"
+  PORTFOLIO_USER: "Your Portfolios",
+  TRANSACTIONS_TABLE_TITLE: "Transactions"
 }
 
 export const LABEL_BUTTONS = {
