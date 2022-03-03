58fbfc578008ea54d8c0be33d139da3c86d2497b
RoanArraes <roan.arraes@hotmail.com>
2022-03-02 21:34:11 -0300

    adjusted table transactions

--- src/components/table-transactions/styles.tsx
+++ src/components/table-transactions/styles.tsx
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
+import { PageItemWrapper } from "../../helpers/ItemWrapper";
+import { PageText } from "../../helpers/PageText";
+
+export const TablesWrapper = styled(PageItemWrapper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const TableArea = styled.table`
   width: 100%;
   border-collapse: collapse;
+  margin: 0px 0px 40px 0px;
 
   thead {
     tr {
@@ -24,14 +34,8 @@ export const TableArea = styled.table`
     tr {
       border-bottom: 1px solid #30323F;
       color: ${props => props.theme.colors.white};
-
-      &:last-child {
-        border-bottom: none;
-      }
     }
   }
-
-
 
   td {
     font-family: ${props => props.theme.fonts[2]};
@@ -41,10 +45,25 @@ export const TableArea = styled.table`
     padding: 20px 10px;
     overflow: hidden;
     text-overflow: ellipsis;
-    cursor: default;
 
     &:last-child {
       cursor: pointer;
     }
   }
+`;
+
+export const Label = styled(PageText)`
+  font-family: ${props => props.theme.fonts[1]};
+  font-size: ${props => props.fontSize ? props.theme.fontSizes[props.fontSize] : props.theme.fontSizes.medium1};
+  color: ${props => props.color ? props.color : props.theme.colors.white};
+  text-shadow: ${props => props.textShadow ? props.theme.shadows.lightWhite0 : "none"};
+  text-transform: ${props => props.textUppercase ? "uppercase" : "none"};
+  margin-bottom: 20px;
+`;
+
+export const InfoMessage = styled.div`
+  padding: 0px 0px 20px 0px;
+  margin-bottom: 20px;
+  color: ${props => props.theme.colors.white};
+  text-align: center;
 `;
